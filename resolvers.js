32e23a9c61fcee95c47eb30b6208350bbbdc3859
RdotSilva <RyandotSilva@gmail.com>
2020-01-29 15:12:52 -0500

    Add authenticated component

--- resolvers.js
+++ resolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const user = {
   _id: "1",
   name: "Ryan",
@@ -5,8 +7,16 @@ const user = {
   picture: "www.fake.com"
 };
 
+// HOC to check if currentUser is inside of context.
+const authenticated = next => (root, args, ctx, info) => {
+  if (!ctx.currentUser) {
+    throw new AuthenticationError("You must be logged in");
+  }
+  return next();
+};
+
 module.exports = {
   Query: {
-    me: () => user
+    me: authenticated((root, args, ctx) => ctx.currentUser)
   }
 };
