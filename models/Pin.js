0f96a46e2f4c28e3418303e0ea03ba10040c1e70
RdotSilva <RyandotSilva@gmail.com>
2020-01-29 14:04:12 -0500

    Add Pin model

---
+++ models/Pin.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const PinSchema = new mongoose.Schema(
+  {
+    title: String,
+    content: String,
+    image: String,
+    latitude: Number,
+    longitude: Number,
+    // Populate author from user data by using the user ID
+    author: { type: mongoose.Schema.ObjectID, ref: "User" },
+    comments: [
+      {
+        text: String,
+        createdAt: { type: Date, default: Date.now },
+        // Populate author from user data by using the user ID
+        author: { type: mongoose.Schema.ObjectID, ref: "User" }
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Pin", PinSchema);
