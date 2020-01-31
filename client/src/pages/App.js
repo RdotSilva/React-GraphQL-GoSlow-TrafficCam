d7e13727cae7b30b322e9457364e9c0f635cecdc
RdotSilva <RyandotSilva@gmail.com>
2020-01-31 16:52:24 -0500

    Add Map to main App

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import withRoot from "../withRoot";
 import Header from "../components/Layout/Header";
+import Map from "../components/Map/Map";
 
 const App = () => {
-  return <Header></Header>;
+  return (
+    <>
+      <Header />
+      <Map />
+    </>
+  );
 };
 
 export default withRoot(App);
