45a88492a9d33c117e5706a389b7e975ab4902a9
RdotSilva <RyandotSilva@gmail.com>
2020-01-31 17:38:45 -0500

    Update PinIcon to show label

--- client/src/components/Map/PinIcon.js
+++ client/src/components/Map/PinIcon.js
@@ -1,8 +1,23 @@
 import React from "react";
 import PlaceTwoTone from "@material-ui/icons/PlaceTwoTone";
+import { withStyles } from "@material-ui/core/styles";
 
-const PinIcon = ({ size, color, onClick }) => (
-  <PlaceTwoTone onClick={onClick} style={{ fontSize: size, color }} />
+const PinIcon = ({ size, color, onClick, label, classes }) => (
+  <div className={classes.center}>
+    <PlaceTwoTone onClick={onClick} style={{ fontSize: size, color }} />
+    <div>{label}</div>
+  </div>
 );
+const styles = {
+  center: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontWeight: "bold"
+  }
+};
 
-export default PinIcon;
+export default withStyles(styles)(PinIcon);
