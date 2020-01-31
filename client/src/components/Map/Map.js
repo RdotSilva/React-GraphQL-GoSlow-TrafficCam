6e77551f25056f659bfc70e53ea1d7d9f9edc5bd
RdotSilva <RyandotSilva@gmail.com>
2020-01-31 17:00:20 -0500

    Add Map component

---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,53 @@
+import React from "react";
+import ReactMapGL from "react-map-gl";
+import { withStyles } from "@material-ui/core/styles";
+// import Button from "@material-ui/core/Button";
+// import Typography from "@material-ui/core/Typography";
+// import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
+
+const Map = ({ classes }) => {
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapboxApiAccessToken="pk.eyJ1IjoicmRvdHNpbHZhIiwiYSI6ImNrNjJwNHl3NjBndGEzZXIwNDVrM3hhOXAifQ.xyMmyqimZBA6XtUG7oUUjQ"
+        {...viewport}
+      ></ReactMapGL>
+    </div>
+  );
+};
+
+const styles = {
+  root: {
+    display: "flex"
+  },
+  rootMobile: {
+    display: "flex",
+    flexDirection: "column-reverse"
+  },
+  navigationControl: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    margin: "1em"
+  },
+  deleteIcon: {
+    color: "red"
+  },
+  popupImage: {
+    padding: "0.4em",
+    height: 200,
+    width: 200,
+    objectFit: "cover"
+  },
+  popupTab: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column"
+  }
+};
+
+export default withStyles(styles)(Map);
