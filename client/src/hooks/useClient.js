9646b3b633c0cc2dcfd455296d21a0cb114c8ef6
RdotSilva <RyandotSilva@gmail.com>
2020-02-02 16:42:10 -0500

    Add useClient custom hook

---
+++ client/src/hooks/useClient.js
@@ -0,0 +1,27 @@
+// Custom hook that will create a new GraphQL client instance to use inside of any
+// other component to send GraphQL requests.
+import React, { useState, useEffect } from "react";
+import { GraphQLClient } from "graphql-request";
+
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "production-url"
+    : "http://localhost:4000/graphql";
+
+export const useClient = () => {
+  const [idToken, setIdToken] = useState("");
+
+  useEffect(() => {
+    // Get token from window and set token state.
+    const token = window.gapi.auth2
+      .getAuthInstance()
+      .currentUser.get()
+      .getAuthResponse().id_token;
+
+    setIdToken(token);
+  }, []);
+
+  return new GraphQLClient(BASE_URL, {
+    headers: { authorization: idToken }
+  });
+};
