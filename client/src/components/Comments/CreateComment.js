945ae8239823e01e88acbd65d499f201ac1e4199
RdotSilva <RyandotSilva@gmail.com>
2020-02-05 18:15:04 -0500

    Add CreateComment styles

--- client/src/components/Comments/CreateComment.js
+++ client/src/components/Comments/CreateComment.js
@@ -10,4 +10,23 @@ const CreateComment = ({ classes }) => {
   return <div>CreateComment</div>;
 };
 
+const styles = theme => ({
+  form: {
+    display: "flex",
+    alignItems: "center"
+  },
+  input: {
+    marginLeft: 8,
+    flex: 1
+  },
+  clearButton: {
+    padding: 0,
+    color: "red"
+  },
+  sendButton: {
+    padding: 0,
+    color: theme.palette.secondary.dark
+  }
+});
+
 export default withStyles(styles)(CreateComment);
