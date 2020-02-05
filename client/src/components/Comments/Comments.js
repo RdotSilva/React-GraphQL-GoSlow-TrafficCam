b3254c2b36b38d51b7874d1fc6eaa0a91b2f186a
RdotSilva <RyandotSilva@gmail.com>
2020-02-05 18:13:55 -0500

    Add Comments styles

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -9,4 +9,14 @@ import { withStyles } from "@material-ui/core/styles";
 
 const Comments = ({ classes }) => <div>Comments</div>;
 
+const styles = theme => ({
+  root: {
+    width: "100%",
+    backgroundColor: theme.palette.background.paper
+  },
+  inline: {
+    display: "inline"
+  }
+});
+
 export default withStyles(styles)(Comments);
