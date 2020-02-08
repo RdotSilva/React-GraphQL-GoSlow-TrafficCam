bbb7ff00627922a6ac6ad36a7593351e1fb1864c
RdotSilva <RyandotSilva@gmail.com>
2020-02-08 11:46:49 -0500

    Add Legend component

---
+++ client/src/components/Map/Legend.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import PlaceTwoTone from "@material-ui/icons/PlaceTwoTone";
+
+const Legend = () => {
+  return <div></div>;
+};
+
+export default withStyles(styles)(Legend);
