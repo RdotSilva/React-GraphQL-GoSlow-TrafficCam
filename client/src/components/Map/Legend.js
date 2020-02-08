ef0fd0a76bb70ac83137bac2a86025426a596101
RdotSilva <RyandotSilva@gmail.com>
2020-02-08 12:11:49 -0500

    Add speedCam styles

--- client/src/components/Map/Legend.js
+++ client/src/components/Map/Legend.js
@@ -40,6 +40,12 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+  },
+  speedCam: {
+    color: "#262730",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
   }
 };
 
