5006aeb3d2f5eafbf3d68675dcca73bf1a586b87
RdotSilva <RyandotSilva@gmail.com>
2020-02-11 20:03:07 -0500

    Change signout link & icon color

--- client/src/components/Auth/Signout.js
+++ client/src/components/Auth/Signout.js
@@ -40,11 +40,11 @@ const styles = {
     display: "flex"
   },
   buttonText: {
-    color: "orange"
+    color: "black"
   },
   buttonIcon: {
     marginLeft: "5px",
-    color: "orange"
+    color: "black"
   }
 };
 
